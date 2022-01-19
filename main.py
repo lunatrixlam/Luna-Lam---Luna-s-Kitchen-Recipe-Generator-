c0d65900225177a4dd12fce53035c12734524a49
Luna Lam <93636920+lunatrixlam@users.noreply.github.com>
2022-01-19 23:00:10 +0000

    Added additional functions.

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 """
-Luna's Kitchen is a recipe generator program that uses the requests and BeautifulSoup4 modules from bs4 to webscrape data from the web to populate a reciple title, recipe description summary, link to recipe URL, and recipe details including serving size, ingredients list, and instructions list.
+Luna's Kitchen is a recipe generator program that uses the requests and BeautifulSoup4 modules from bs4 to webscrape data from the web to populate a recipe title, recipe description summary, link to recipe URL, and recipe details including serving size, ingredients list, and instructions list. Users also have the ability to save the recipe and URL to a dictionary and save recipe ingredients to a shopping list.
 """
 
 from bs4 import BeautifulSoup
 
 import requests
+
+# A dictionary to store saved recipes
+my_recipes = {}
+
+# A shopping list to store saved recipe ingredients
+my_shopping_list = []
 
 # The draw_border function creates a pretty border
 def draw_border():
@@ -16,9 +22,18 @@ def welcome():
     print(("\U0001F319 " * 3) + " Welcome to Luna's Kitchen " + (" \U0001F319" * 3))   # \U000 allows for emoji printing via unicode
     draw_border() # creates a decorative border
 
+# The display_menu function displays the navigation options to the user
+def display_menu():
+    print("Menu Options:")
+    print("[V]iew Saved Recipes")
+    print("[G]et a Recipe")
+    print("[D]isplay Shopping List")
+    print("[Q]uit Kitchen")
+    print()
+
 # The goodbye function wishes the user goodbye
 def say_goodbye():
-    print(("\U0001F319 ") + " Thanks for visiting Luna's Kitchen! " + (" \U0001F319"))
+    print(("\U0001F319 ") + " Thanks for visiting Luna's Kitchen! Goodbye. " + (" \U0001F319"))
 
 # The get_recipe function  asks the user for input to generate a URL to allow the calling of requests, BeautifulSoup, and find methods to generate recipe title, summary, and link. 
 
@@ -40,6 +55,10 @@ def get_recipe():
     result = requests.get(url)  # sends HTTP GET request to URL to scrape, responding with HTML content of page
 
     doc = BeautifulSoup(result.text, "html.parser") # fetches and parses the HTML content data as text
+
+    draw_border()
+
+    global recipe_title
 
     recipe_title = doc.find("h3", class_="card__title").text    # analyzes h3 tag and class attributes to extract specified data
 
@@ -80,6 +99,8 @@ def view_recipe():
             print(recipe_servings)
             print()
 
+            global ingredients
+
             ingredients = recipe_doc.find("fieldset", class_="ingredients-section__fieldset").text
 
             # ln 84 to 92 cleans up the output by removing unnecessary whitespace from raw website output
@@ -119,6 +140,8 @@ def view_recipe():
             
             instructions = update_instructions
 
+            draw_border()
+
             for i in instructions:
                 print(i)
                 print()
@@ -127,6 +150,91 @@ def view_recipe():
             break
         else:
             print("Sorry, that wasn't an option. Try again.")
+            print()
+
+# The my_saved_recipes function displays the saved recipes from the user's current session in Luna's Kitchen
+
+def my_saved_recipes():
+
+    if my_recipes == {}:
+        print("No recipes currently saved.")
+    else:
+        print("\U0001F4D6 My Recipes:")
+        
+        count = 1
+        for key in my_recipes:
+            print(f"{count}: {key}, {my_recipes[key]}")
+            count += 1    
+    print()
+
+# The save_ingredients function adds the ingredients from the most recently viewed recipe to my_shopping_list and also checks to see if the same recipe's ingredients were previously already added
+
+def save_ingredients():
+
+    while True:
+
+        save_ingredient = input("Would you like to save the recipe ingredients to your shopping list? Enter yes or no: \n> ").lower().strip()
+        print()
+
+        if save_ingredient == "n" or save_ingredient == "no":
+            break
+        elif save_ingredient == "y" or save_ingredient == "yes":
+
+            in_list = False
+
+            for item in my_shopping_list:
+                if item == recipe_title.strip():
+                    in_list = True
+            
+            if in_list == True:
+                print("Recipe ingredients previously added to shopping list!")
+                print()
+
+            if in_list == False:
+                my_shopping_list.append(recipe_title)
+                for ingredient in ingredients:
+                    my_shopping_list.append(ingredient)
+                print(f"\U0001F389 Success! {recipe_title.strip()} recipe ingredients saved to shopping list.")
+                print()
+            break
+        else:
+            print("Sorry, that wasn't an option. Try again.")
+            print()
+
+
+# The save_recipe function saves the most recently viewed recipe into my_recipes dictionary with the "recipe_title: recipe_link" as the "key: value" pair
+
+def save_recipe():
+
+    while True:
+        
+        save_recipe = input("Would you like to save this recipe for later? Enter yes or no: \n> ").lower().strip()
+        print()
+
+        if save_recipe == "n" or save_recipe == "no":
+            break
+        elif save_recipe == "y" or save_recipe == "yes":
+            my_recipes[recipe_title.strip()] = recipe_link.strip()
+
+            print(f"\U0001F389 Success: {recipe_title.strip()} saved to your recipes.")
+            print()        
+
+            save_ingredients()
+            break
+        else:
+            print("Sorry, that wasn't an option. Try again.")
+            print()
+
+# The display_shopping_list function displays the current list items in my_shopping_list to the user 
+
+def display_shopping_list():
+    if my_shopping_list == []:
+        print("No items in shopping list.")
+    else:
+        print("\U0001F4DD My Shopping List:")
+        for item in my_shopping_list:
+            print(f"{item}")
+    print()
 
 # The start_kitchen function starts up the program and walks the user through the navigation options throughout the program.
 
@@ -135,15 +243,22 @@ def start_kitchen():
     welcome()
 
     while True:
-        are_ready = input("Are you ready to start cooking? Enter yes or no: \n> ").lower().strip()
+        display_menu()
+
+        select_menu = input("Enter menu option: \n> ").lower().strip()
         print()
 
-        if are_ready == "n" or are_ready == "no":
+        if select_menu == "q" or select_menu == "quit":
             say_goodbye()
             break
-        elif are_ready == "y" or are_ready == "yes":
+        elif select_menu == "v" or select_menu == "view":
+            my_saved_recipes()
+        elif select_menu == "g" or select_menu == "get":
             get_recipe()
             view_recipe()
+            save_recipe()
+        elif select_menu == "d" or select_menu == "display":
+            display_shopping_list()
         else:
             print("Sorry, that wasn't an option. Try again.")
             print()
